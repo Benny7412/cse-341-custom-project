ecec29860c3e7da7f9e3024a81e17c5f64ad1e6c
Benny7412 <106467418+Benny7412@users.noreply.github.com>
2025-08-02 17:04:55 -0500

    fix

--- routes/docs/swagger.js
+++ routes/docs/swagger.js
@@ -1,21 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const scheme = process.env.SCHEME || 'http';
+const host = process.env.BASE_URL || 'localhost:8080';
+
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
     title: 'Cats API',
-    description: 'API for managing cats and cat breeds information'
+    description: 'API for managing cats and cat breeds information',
+    host
   },
   host: 'cse-341-custom-project.onrender.com',
   basePath: '/',
-  schemes: ['https'],
+  schemes: scheme,
 };
 
 const outputFile = 'routes/docs/swagger.json';
-const endpointsFiles = [
-    '../index.js',
-    '../cats.js',
-    '../catBreeds.js'
-];
+const endpointsFiles = ['../index.js'];
 
 // Generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const getHome = require("../controllers/index");
 
 router.get("/", getHome);
-//router.use("/cats", require("./cats"));
-//router.use("/catBreeds", require("./catBreeds"));
+router.use("/cats", require("./cats"));
+router.use("/catBreeds", require("./catBreeds"));
 
 module.exports = router;
