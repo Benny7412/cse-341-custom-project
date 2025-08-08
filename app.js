8e5b229b04d662c31732852bc9fa0a4684f2bfe5
Benny7412 <bennyklaus@gmail.com>
2025-08-08 03:34:58 -0500

    X

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-require('dotenv').config();
-const path       = require('path');
-const express    = require('express');
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
 
 // MongoDB connection
 const mongodb = require("./data/database");
 
 // Import routes
-const indexRoutes = require('./routes/index');
-const catsRoutes = require('./routes/cats');
-const catBreedsRoutes = require('./routes/catBreeds');
+const indexRoutes = require("./routes/index");
+const catsRoutes = require("./routes/cats");
+const catBreedsRoutes = require("./routes/catBreeds");
 
 //authentication
-const passport = require('passport');
-const session = require('express-session');
-const GithubStrategy = require('passport-github').Strategy;
-const cors = require('cors');
+const passport = require("passport");
+const session = require("express-session");
+const GithubStrategy = require("passport-github").Strategy;
+const cors = require("cors");
 
-// Init express 
+// Init express
 const app = express();
 
 /* ---------- swagger docs ---------- */
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./routes/docs/swagger.json');
 
-
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./routes/docs/swagger.json");
 
 /* ---------- middleware ---------- */
 
@@ -33,41 +31,54 @@ app
   //serve public
   .use(express.static(path.join(__dirname, "public")))
   //session middleware
-  .use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })) 
+  .use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+  )
   .use(passport.initialize())
   .use(passport.session())
 
   // CORS headers
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, OPTIONS, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+    );
     next();
   })
-  .use(cors({ methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'DELETE'] }))
-  .use(cors({ origin: '*' }))
+  .use(cors({ methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"] }))
+  .use(cors({ origin: "*" }))
   .use("/", require("./routes/index.js"));
 
 /* ---------- routes ---------- */
 app.use("/", indexRoutes);
 app.use("/cats", catsRoutes);
 app.use("/catBreeds", catBreedsRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/api-docs/login", passport.authenticate("github"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // CORS headers
 
-passport.use(new GithubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL,
-},
-function(accessToken, refreshToken, profile, done) {
-  return done(null, profile);
- }));
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -76,22 +87,32 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : "Logged Out"
+  );
+});
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out" )});
-
-app.get('/github/callback',
-  passport.authenticate('github', { failureRedirect: '/api-docs', session: false }),
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
   (req, res) => {
     req.session.user = req.user;
-    res.redirect('/');
-  });
-
+    res.redirect("/");
+  }
+);
 
 /* ---------- start server ---------- */
 const port = process.env.PORT || 8080;
-mongodb.initDb()
+mongodb
+  .initDb()
   .then((client) => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,28 @@ const router = express.Router();
 const getHome = require("../controllers/index");
 const passport = require("passport");
 
-router.get("/", getHome);
+router.get("/login", passport.authenticate("github"));
 router.use("/cats", require("./cats"));
 router.use("/catBreeds", require("./catBreeds"));
 
-router.get('login', passport.authenticate('github', (req, res) => {}));
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) {return next(err); }
-        res.redirect('/');
-    });
- });
+router.get("/login", (req, res, next) => {
+  const dynamicCallback = `${req.protocol}://${req.get(
+    "host"
+  )}/github/callback`;
+  passport.authenticate("github", { callbackURL: dynamicCallback })(
+    req,
+    res,
+    next
+  );
+});
+
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
