4f251fb7a7577acae47eab661ce31b880175456d
Benny7412 <bennyklaus@gmail.com>
2025-08-14 01:18:59 -0500

    cutting out the prod switch

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,7 @@ const passport = require("passport");
 router.use("/cats", require("./cats"));
 router.use("/catBreeds", require("./catBreeds"));
 
-router.get("/login", (req, res, next) => {
-  const dynamicCallback = `${req.protocol}://${req.get(
-    "host"
-  )}/github/callback`;
-  passport.authenticate("github", { callbackURL: dynamicCallback })(
-    req,
-    res,
-    next
-  );
-});
+router.get("/login", passport.authenticate("github"));
 
 router.get("/logout", (req, res, next) => {
   req.logout(function (err) {
